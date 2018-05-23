1463613c151d60f3867f422e9649af81c86afc79
Danny Chen <38952310+HamSpaghet@users.noreply.github.com>
2018-05-23 13:14:25 +0200

    Add files via upload
    Simple distance sensor code with credits to https://howtomechatronics.com/tutorials/arduino/ultrasonic-sensor-hc-sr04/

---
+++ Distance_Sensor_Code/Distance_Sensor_Code.ino
@@ -0,0 +1,54 @@
+ #define seconde 1800
+
+  // Goal = Making Ultrasonic sensor working
+  // defines pins numbers
+  const int trigPin = 6;
+  const int echoPin = 7;
+
+  //LED RED
+  int LEDR = 23;
+  //LED GREEN
+  int LEDG = 22;
+  //LED ORANGE
+  int LEDO = 24;
+  // defines variables
+  long duration1;
+  int distance1;
+  long duration;
+  int distance;
+  
+void setup() {
+  
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+  
+}
+
+void loop(){  
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  delayMicroseconds(2);
+    
+  digitalWrite(trigPin, HIGH);
+
+  delayMicroseconds(10);
+   
+  digitalWrite(trigPin, LOW);
+    
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance
+  distance = duration*0.034/2;
+  
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  
+  led(distance);
+  
+}
+
+
---
+++ Distance_Sensor_Code/LED.ino
@@ -0,0 +1,22 @@
+#define dichtbij 20
+#define veryclose 10
+void led(int distance){
+
+    if(distance > dichtbij){
+    digitalWrite(LEDG, LOW);
+    digitalWrite(LEDO, LOW);
+    digitalWrite(LEDR, HIGH);
+  }else if(distance <= dichtbij){
+    if(distance >= veryclose){
+      digitalWrite(LEDR, LOW);
+      digitalWrite(LEDG, LOW);
+      digitalWrite(LEDO, HIGH);
+    }else{
+      digitalWrite(LEDR, LOW);
+      digitalWrite(LEDO, LOW);
+      digitalWrite(LEDG, HIGH);
+    }
+  }
+  return;
+}
+
